2bd28851d0b8147471b44c96e6934d98c097861f
Frank Gerold <FrankETSGerold@gmail.com>
2020-03-01 17:39:21 -0500

    part 1 complete. Player moves around with arrows

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod
+from input_handlers import handle_keys
 
 def main():
     print('Suhh dude')
@@ -13,15 +14,38 @@ def main():
 
     tcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
+    con = tcod.console_new(screen_width, screen_height)
+
+    key = tcod.Key()
+    mouse = tcod.Mouse()
+
     while not tcod.console_is_window_closed():
-        tcod.console_set_default_foreground(0, tcod.red)
-        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
+        tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
+
+        tcod.console_set_default_foreground(con, tcod.red)
+        tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
+        tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         tcod.console_flush()
 
-        key = tcod.console_check_for_keypress()
+        # Without this, character wouldnt clear after movement, ended up snaking out. this clears previous spot.
+        tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
 
-        if key.vk == tcod.KEY_ESCAPE:
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
             return True
+
+        if fullscreen:
+            tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
 
 if __name__ == '__main__':
     main()
