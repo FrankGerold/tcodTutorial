5807ad7976766a48e52f9c04789310e3801e81be
Frank Gerold <FrankETSGerold@gmail.com>
2020-03-01 17:15:59 -0500

    player is centered

--- engine.py
+++ engine.py
@@ -2,15 +2,20 @@ import tcod
 
 def main():
     print('Suhh dude')
+
     screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
 
     tcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     while not tcod.console_is_window_closed():
-        tcod.console_set_default_foreground(0, tcod.white)
-        tcod.console_put_char(0, 1, 1, '@', tcod.BKGND_NONE)
+        tcod.console_set_default_foreground(0, tcod.red)
+        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
         tcod.console_flush()
 
         key = tcod.console_check_for_keypress()
