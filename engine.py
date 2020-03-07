61ba0b6de0ec239f4f6e23841d1764b41c5c66c5
Frank Gerold <FrankETSGerold@gmail.com>
2020-03-07 11:50:50 -0500

    Populate rooms with monsters

--- engine.py
+++ engine.py
@@ -17,6 +17,8 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    max_monsters_per_room = 3
+
     fov_algorithm = 0
     fov_light_walls = True
     fov_radius = 10
@@ -38,7 +40,7 @@ def main():
     con = tcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, npc)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room)
 
     fov_recompute = True
     fov_map = initialize_fov(game_map)
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,6 +1,8 @@
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from random import randint
+import tcod
+from entity import Entity
 
 class GameMap:
     def __init__(self, width, height):
@@ -20,7 +22,7 @@ class GameMap:
 
         return tiles
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, npc):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room):
         # #Create 2 rooms for demo
         # room1 = Rect(20, 15, 10, 15)
         # room2 = Rect(35, 15, 10, 15)
@@ -77,6 +79,8 @@ class GameMap:
                         self.create_h_tunnel(prev_x, new_x, new_y)
                         self.create_v_tunnel(prev_y, new_y, prev_x)
 
+                self.place_entities(new_room, entities, max_monsters_per_room)
+
                 #Finally, append new room to list
                 rooms.append(new_room)
                 num_rooms += 1
@@ -97,6 +101,25 @@ class GameMap:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        #Get random # monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            #choose rand location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 +1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                # 80% cnance for orc
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', tcod.desaturated_green)
+                # 20% chance for troll
+                else:
+                    monster = Entity(x, y, 'T', tcod.darker_green)
+
+                entities.append(monster)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
